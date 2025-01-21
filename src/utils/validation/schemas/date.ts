d74b2b735a127acfe0e2491ed0ad878481c1e55a
yuriy.podkopaev <yuriy.podkopaev@spbpu.com>
2025-01-21 17:31:55 +0300

    fix tests

--- src/utils/validation/schemas/date.ts
+++ src/utils/validation/schemas/date.ts
@@ -1,16 +1,13 @@
 import { isPast, isValid } from 'date-fns';
 import * as y from 'yup';
 
-import { getCurrentUserTimezone } from '~/app/user/user.store';
-import { applyTzOffset } from '~/utils/date/apply-tz-offset';
-
 export const dateMinSchema = y.date().test({
   name: 'is-min-date',
   test: (date, ctx) => {
     const isValidDate = date instanceof Date && isValid(date);
 
     if (isValidDate && isPast(date)) {
-      const currentDate = new Date(applyTzOffset(new Date().toJSON(), getCurrentUserTimezone()));
+      const currentDate = new Date();
       return ctx.createError({
         message: { key: 'yup:date.min', values: { min: currentDate.toLocaleString() } },
       });
