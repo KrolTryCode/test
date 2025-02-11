f54e662aaba71d9907e72c929b4f211694cce05b
Кристина Андреева <kristina.andreeva@spbpu.com>
2025-02-11 12:01:27 +0000

    FE-119: [Проект - Таблицы] Некорректная работа валидации при редактировании данных + FE-134: [Проект - Редактирование структуры таблицы] Изменение чекбокса "Необязательное поле"

--- src/components/forms/table-data/table-data-form.schema.tsx
+++ src/components/forms/table-data/table-data-form.schema.tsx
@@ -2,66 +2,68 @@ import * as y from 'yup';
 
 import { DataType, TableColumn } from '~/api/utils/api-requests';
 
-export const getSchema = (properties: TableColumn[], tableContent: Record<string, any>[]) =>
-  y.object().shape(
-    Object.fromEntries(
-      properties.map(prop => {
-        const isRequired = !prop.nullable;
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        const forbiddenValues = prop.unique ? tableContent?.map(row => row[prop.id]) : [];
-        switch (prop.type) {
-          case DataType.Float:
-            return [
-              prop.id,
-              y
-                .number()
-                .when(() =>
-                  (isRequired ? y.number().required() : y.number().notRequired()).notOneOf(
-                    forbiddenValues,
-                  ),
-                ),
-            ];
-          case DataType.Int:
-            return [
-              prop.id,
-              y
-                .number()
-                .when(() =>
-                  (isRequired ? y.number().required() : y.number().notRequired())
-                    .integer()
-                    .notOneOf(forbiddenValues),
-                ),
-            ];
-          case DataType.Boolean:
-            return [
-              prop.id,
-              y
-                .boolean()
-                .when(() =>
-                  (isRequired ? y.boolean().required() : y.boolean().notRequired()).notOneOf(
-                    forbiddenValues,
-                  ),
-                ),
-            ];
-          case DataType.Timestamp:
-          case DataType.Date:
-          case DataType.LineString:
-          case DataType.Point:
-          case DataType.Polygon:
-          case DataType.Uuid:
-          case DataType.String:
-          default:
-            return [
-              prop.id,
-              y
-                .string()
-                .when(() =>
-                  (isRequired ? y.string().required() : y.string().notRequired()).notOneOf(
-                    forbiddenValues,
-                  ),
-                ),
-            ];
+const buildPropSchema = (
+  prop: TableColumn,
+  tableContent: Record<string, string | number | boolean | null | undefined>[],
+) => {
+  let schema: y.AnySchema;
+  switch (prop.type) {
+    case DataType.Float:
+      schema = y.number();
+      break;
+    case DataType.Int:
+      schema = y.number().integer();
+      break;
+    case DataType.Boolean:
+      schema = y.boolean();
+      break;
+    case DataType.Timestamp:
+    case DataType.Date:
+    case DataType.LineString:
+    case DataType.Point:
+    case DataType.Polygon:
+    case DataType.Uuid:
+    case DataType.String:
+    default:
+      schema = y.string();
+      break;
+  }
+
+  if (prop.unique) {
+    schema = schema.test({
+      name: 'is-unique',
+      test: (value: string | number | boolean | null | undefined, ctx) => {
+        const forbiddenValues = tableContent
+          ?.filter(row => row.id !== ctx.parent.id && row[prop.id])
+          .map(row => row[prop.id]);
+
+        const isEmpty = value === null || value === undefined || value === '';
+        if (!isEmpty && forbiddenValues.includes(value)) {
+          return ctx.createError({
+            message: { key: 'yup:mixed.notOneOf', values: { values: forbiddenValues } },
+          });
         }
-      }),
-    ),
+
+        return true;
+      },
+    });
+  }
+
+  if (!prop.nullable) {
+    schema = schema.required() as y.AnySchema;
+  }
+
+  return schema;
+};
+
+export const getSchema = (properties: TableColumn[], tableContent: Record<string, any>[]) => {
+  const shape = properties.reduce(
+    (acc, prop) => {
+      acc[prop.id] = buildPropSchema(prop, tableContent);
+      return acc;
+    },
+    {} as Record<string, any>,
   );
+
+  return y.object().shape(shape);
+};
