3b1ac82010d87626af732a8091c6a44e78ac1058
Kristina Andreeva <kristina.andreeva@spbpu.com>
2025-03-14 14:55:33 +0300

    FE-160: [Проект - Расчеты] Не работает редактирование расчета

--- src/components/forms/task/task-form.schema.ts
+++ src/components/forms/task/task-form.schema.ts
@@ -7,17 +7,19 @@ export interface ITaskForm {
   name: string;
   description?: string;
   formId: string;
-  parameters: Omit<RealFullTaskInfo['parameters']['params'], 'timeout'>;
+  parameters?: Omit<RealFullTaskInfo['parameters']['params'], 'timeout'>;
 }
+
+const parameterSchema: y.ObjectSchema<ITaskForm['parameters']> = y.object({
+  solver: UUIDSchema.required(),
+  contents: y.array(UUIDSchema).min(1, { key: 'yup:mixed.required' }).required().default([]),
+  timeout: y.string().required(),
+});
 
 export const getSchema = (isEditing: boolean): y.ObjectSchema<ITaskForm> =>
   y.object({
     name: y.string().required().default(''),
     description: y.string().default(''),
     formId: isEditing ? y.string().uuid().default('') : y.string().uuid().required().default(''),
-    parameters: y.object({
-      solver: UUIDSchema.required(),
-      contents: y.array(UUIDSchema).min(1, { key: 'yup:mixed.required' }).required().default([]),
-      timeout: y.string().required(),
-    }),
+    parameters: isEditing ? y.mixed() : parameterSchema,
   });
