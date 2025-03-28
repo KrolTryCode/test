8fcb5b2a73baf8df655e6a11ea5ca458924d8639
Sergei Antsiferov <sergei.antsiferov@spbpu.com>
2025-03-28 13:17:41 +0000

    [FE-143] [Проект - Таблицы] Проверка ограничений таблицы при добавлении записей

--- src/components/forms/table-data/schema/table-data-form.schema.tsx
+++ src/components/forms/table-data/schema/table-data-form.schema.tsx
@@ -1,18 +1,26 @@
 import * as y from 'yup';
 
-import { DataType, TableColumn } from '~/api/utils/api-requests';
+import { DataType, TableCheck, TableColumn } from '~/api/utils/api-requests';
+import { numberDataRestrictionsSchema } from '~/components/forms/table-data/schema/data-restrictions/number-schema';
+import { stringDataRestrictionsSchema } from '~/components/forms/table-data/schema/data-restrictions/string-schema';
 
 const buildPropSchema = (
   prop: TableColumn,
   tableContent: Record<string, string | number | boolean | null | undefined>[],
+  checks: TableCheck[],
+  columns: TableColumn[],
 ) => {
   let schema: y.AnySchema;
+
   switch (prop.type) {
-    case DataType.Float:
-      schema = y.number();
+    case DataType.String:
+      schema = stringDataRestrictionsSchema(checks);
       break;
     case DataType.Int:
-      schema = y.number().integer();
+      schema = numberDataRestrictionsSchema(checks, columns).integer();
+      break;
+    case DataType.Float:
+      schema = numberDataRestrictionsSchema(checks, columns);
       break;
     case DataType.Boolean:
       schema = y.boolean();
@@ -23,7 +31,6 @@ const buildPropSchema = (
     case DataType.Point:
     case DataType.Polygon:
     case DataType.Uuid:
-    case DataType.String:
     default:
       schema = y.string();
       break;
@@ -56,10 +63,14 @@ const buildPropSchema = (
   return schema;
 };
 
-export const getSchema = (properties: TableColumn[], tableContent: Record<string, any>[]) => {
-  const shape = properties.reduce(
+export const getSchema = (
+  columns: TableColumn[],
+  tableContent: Record<string, any>[],
+  checks: TableCheck[],
+) => {
+  const shape = columns.reduce(
     (acc, prop) => {
-      acc[prop.id] = buildPropSchema(prop, tableContent);
+      acc[prop.id] = buildPropSchema(prop, tableContent, checks, columns);
       return acc;
     },
     {} as Record<string, any>,
